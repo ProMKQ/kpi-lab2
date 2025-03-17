35c6d8163d8754d4af17b1b0bec6b339af3212e7
ProMKQ <maximik128@gmail.com>
2025-03-17 22:13:14 +0200

    Add basic postfix to infix conversion

--- implementation.go
+++ implementation.go
@@ -1,9 +1,47 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+type stack []string
+
+func (s *stack) Push(v string) {
+	*s = append(*s, v)
+}
+
+func (s *stack) Pop() string {
+	l := len(*s)
+	v := (*s)[l-1]
+	*s = (*s)[:l-1]
+	return v
+}
+
+// PostfixToInfix converts an expression from Reverse Polish (postfix) notation to infix notation
+func PostfixToInfix(input string) (string, error) {
+	tokens := strings.Fields(input)
+	s := make(stack, 0)
+
+	for _, token := range tokens {
+		if token == "+" || token == "-" || token == "*" || token == "/" || token == "^" {
+			if len(s) < 2 {
+				return "", fmt.Errorf("expected at least 2 tokens, got %d", len(s))
+			}
+			right := s.Pop()
+			left := s.Pop()
+			s.Push("(" + left + " " + token + " " + right + ")")
+		} else if _, err := strconv.Atoi(token); err == nil {
+			s.Push(token)
+		} else {
+			return "", fmt.Errorf("invalid token: '%s'", token)
+		}
+	}
+
+	if len(s) != 1 {
+		return "", fmt.Errorf("invalid postfix expression")
+	}
+
+	return s[0], nil
 }
